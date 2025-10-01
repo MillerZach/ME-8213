ee2b8eb0924f2abbc2405b6bec819e0e9079a413
liber <liberatorz43@gmail.com>
2025-10-01 07:23:05 -0500

    only lack 3

--- zam108_HW3.py
+++ zam108_HW3.py
@@ -39,6 +39,23 @@ def curvature(xData,yData):
     LUsolve3(c,d,e,k)
     return k
 
+def slope(xData,yData):
+    n = len(xData) - 1
+    c = np.zeros(n)
+    d = np.ones(n+1)
+    e = np.zeros(n)
+    k = np.zeros(n+1)
+    c[0:n-1] = xData[0:n-1] - xData[1:n]
+    d[1:n] = 2.0 * (xData[0:n-1] - xData[2:n+1])
+    e[1:n] = xData[1:n] - xData[2:n+1]
+    k[1:n] = ((xData[0] - xData[1])/3) \
+        + (xData[0] - xData[1])/6 \
+        + (yData[0] - yData[1]) \
+        /(xData[0] - xData[1])
+    LUdecomp3(c,d,e)
+    LUsolve3(c,d,e,k)
+    return k
+
 def evalSpline(xData,yData,k,x):
 
     def findSegment(xData,x):
@@ -99,6 +116,11 @@ yData = np.array([1.0, 1.0, 0.5, 0.0])
 k = curvature(xData,yData)
 print(k)
 
+x = eval(input("\nx ==> "))
+print("y =",evalSpline(xData,yData,k,x))
+
+k = slope(xData,yData)
+print(k)
 x = eval(input("\nx ==> "))
 print("y =",evalSpline(xData,yData,k,x))
 
